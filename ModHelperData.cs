acc3f926e52bba855b75baaa10863b556b619652
WarperSan <78861227+WarperSan@users.noreply.github.com>
2023-01-10 16:59:13 -0500

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+﻿namespace Data
+{
+    public static class ModHelperData
+    {
+        public const string Version = "1.0.0";
+        public const string Name = "Boss Pack";
+
+        public const string Description = "Adds 5 new bosses to the game: The Demon Prince, Queen of Jaws, Bloontonium Expert, Flame of Terror and Ghost King";
+
+        public const string Author = "WarperSan";
+        public const string RepoOwner = "WarperSan";
+        public const string RepoName = "BossPack";
+    }
+}
