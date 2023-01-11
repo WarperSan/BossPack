f68fb6102afee7639f59fd09859aa98e4bc5ed16
WarperSan <78861227+WarperSan@users.noreply.github.com>
2023-01-11 18:03:35 -0500

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@
 {
     public static class ModHelperData
     {
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
         public const string Name = "Boss Pack";
 
         public const string Description = "Adds 5 new bosses to the game: The Demon Prince, Queen of Jaws, Bloontonium Expert, Flame of Terror and Ghost King";
